9f635bd789a18bb71179e077c9a1aff86afb0fe1
gscriva <giuseppe.scriva@unicam.it>
2021-01-15 09:44:12 +0100

    Add model class for MLP

--- functions/model.py
+++ functions/model.py
@@ -1,8 +1,20 @@
-import torch.nn
+import torch.nn as nn
 
 
-class FeedForward(nn.Module):
-    """Model Class"""
+class MultiLayerPerceptron(nn.Module):
+    """Multi Layer Perceptron"""
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, layers):
+        super(MultiLayerPerceptron, self).__init__()
+        sizes = [256] + [150] * layers + [1]
+        fc_layers = []
+        for i in range(len(sizes) - 1):
+            fc_layers.append(nn.Linear(sizes[i], sizes[i + 1]))
+            if i == len(sizes) - 2:
+                continue
+            fc_layers.append(nn.ReLU())
+
+        self.layers = nn.Sequential(fc_layers)
+
+    def forward(self, x):
+        return self.layers(x)
