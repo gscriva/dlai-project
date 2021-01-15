ad4d8fc46d2fa92c9febbd848ea7b227c542e6eb
gscriva <giuseppe.scriva@unicam.it>
2021-01-15 19:43:37 +0100

    Add seed in spliting train and valid set

--- functions/data_loader.py
+++ functions/data_loader.py
@@ -11,25 +11,29 @@ class Speckle(Dataset):
         data_file: str,
         data_name: str,
         output_name: str = "evalues",
-        validation: bool = False,
+        train: bool = False,
         train_size: float = 0.8,
+        seed: int = 0,
     ):
         """
         Args:
             data_file (str): Path to the npz file.
             data_name (str): Key to retrieve the correct array from the file.
             output_name (str, optional): Key to get the energy values. Defaults to 'evalues'.
-            validation (bool, optional): Set True if it has to return the validation set. Defaults to False.
+            train (bool, optional): Set True if it has to return the train set. Defaults to False.
             train_size (float, optional): Set the size of training set. Defaults to 0.8.
+            seed (int, optional): Seed to split the dataset between training and validation set.
         """
         data = np.load(data_file)
         idx = int(floor(len(data[output_name])) * train_size)
-        if validation:
-            self.dataset = data[data_name][idx:, :]
-            self.evalues = data[output_name][idx:]
-        else:
-            self.dataset = data[data_name][:idx, :]
-            self.evalues = data[output_name][:idx]
+        size_ds = len(data[output_name])
+        np.random.seed(0)
+        idx = np.full(size_ds, False, dtype=bool)
+        idx[np.random.choice(size_ds, floor(size_ds * train_size))] = True
+        if not train:
+            idx = np.logical_not(idx)
+        self.dataset = data[data_name][idx, :]
+        self.evalues = data[output_name][idx]
 
     def __len__(self):
         return (self.evalues).size
