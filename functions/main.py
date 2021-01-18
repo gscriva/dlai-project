134ce47dc9ff01ab5754c7e8497c44d8137fb69a
gscriva <giuseppe.scriva@unicam.it>
2021-01-18 10:11:43 +0100

    Complete main for training

--- functions/main.py
+++ functions/main.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 # import torch.nn as nn
 
@@ -7,12 +8,23 @@ from utils import *
 from model import *
 
 
-def main():
+def main(
+    dataset_path,
+    batch_size,
+    num_workers=10,
+    use_gpu=False,
+    train=False,
+    epochs=20,
+    layers=3,
+    learning_rate=0.001,
+    weight_decay=0.03,
+):
 
-    # TODO add parser with args=[use_gpu, layers, (model?)]
+    # TODO add parser with args=[use_gpu, layers, (model?), dataset_path,
+    # batch_size, shuffle, num_workers, learning_rate, train]
 
     # check if GPU is available
-    use_gpu = args.use_gpu and torch.cuda.is_available()
+    use_gpu = use_gpu and torch.cuda.is_available()
 
     # import model and move it to GPU if available
     model = MultiLayerPerceptron(layers)
@@ -20,6 +32,64 @@ def main():
         model = model.cuda()
 
     # import loss and optimizer
-    criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(),)
+    loss_func = torch.nn.MSELoss()
+    opt = torch.optim.Adam(
+        model.parameters(), lr=learning_rate, weight_decay=weight_decay
+    )
 
+    # load training and test dataset
+    train_loader, valid_loader = load_data(
+        dataset_path, batch_size=batch_size, shuffle=True, num_workers=num_workers
+    )
+
+    best_losses = np.infty
+    train_loss = []
+    validate_loss = []
+
+    if train:
+        for epoch in range(epochs):
+            for speckle, energy in train_loader:
+                pred = model(speckle)
+                loss = loss_func(pred, energy)
+                train_loss[epoch] = int(loss)  # check type
+
+                loss.backward()
+                opt.step()
+                opt.zero_grad()
+
+            print("loss: {0}".format(loss_func(model(speckle), energy)))
+
+            model.eval()
+            with torch.no_grad():
+                val_loss = sum(
+                    loss_func(model(speckle), energy)
+                    for speckle, energy in valid_loader
+                )
+                validate_loss[epoch] = val_loss
+
+            checkpoint_dict = {
+                "model_state_dict": model.state_dict(),
+                "optimizer_state_dict": opt.state_dict(),
+                "epoch": epoch,
+                "best_losses": best_losses,
+                "train_loss": train_loss,
+                "validate_loss": validate_loss,
+            }
+
+            # Save checkpoint every 5 epochs or when a better model is produced
+            if val_loss < best_losses:
+                best_losses = val_loss
+                torch.save(checkpoint_dict, "checkpoints/best-model.pth")
+
+            # save model on each epoch
+            if epoch % 5 == 0:
+                torch.save(
+                    checkpoint_dict,
+                    "checkpoints/model-epoch-{}-losses-{:.0f}.pth".format(
+                        epoch, int(val_loss)
+                    ),
+                )
+
+
+if __name__ == "__main__":
+    main()
