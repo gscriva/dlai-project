0b650f0b960a445d76f840c61b1fa55bfa953a9e
gscriva <giuseppe.scriva@unicam.it>
2021-01-19 11:27:49 +0100

    Fix bug in reading speckle in Fourier

--- functions/utils.py
+++ functions/utils.py
@@ -1,7 +1,7 @@
 import os
-import copy
 from multiprocessing import Pool, cpu_count
 from math import floor
+from typing import Any, Callable
 
 import numpy as np
 import torch
@@ -47,27 +47,32 @@ def save_as_npz(
             else:
                 # valid for speckleR, just real
                 paths.append((path, data_size, 1))
-    old_paths = copy.deepcopy(paths)
     # append extra vector with x axis
-    for path in old_paths:
+    extra_paths = []
+    for path in paths:
         filename = os.path.basename(path[0])[:-4]
         if filename == "speckleR":
-            paths.append((path[0], data_size, 0, "x_axis"))
+            extra_paths.append((path[0], data_size, 0, "x_axis"))
         elif filename == "speckleF":
-            paths.append((path[0], data_size, 0, "csi_axis"))
+            extra_paths.append((path[0], data_size, 0, "csi_axis"))
+
     cpu = np.minimum(len(paths), cpu_count() // 2)
     p = Pool(cpu)
-    datas = list(tqdm.tqdm(p.imap(read_arr_help, paths), total=len(paths)))
 
-    results = split_ds(datas, seed=seed, test_size=test_size)
+    # data are in the same files, so to avoid concurrent accesses the loading is split
+    data = list(p.imap(read_arr_help, paths))
+    data.extend(list(p.imap(read_arr_help, extra_paths)))
+
+    results = split_ds(data, seed=seed, test_size=test_size)
 
     for key in results:
         outname = key + "_" + os.path.basename(data_path)
+        print("Saving {0} dataset as {1}".format(key, outname))
         np.savez(str(outname) + ".npz", **{el[1][:]: el[0] for el in results[key]})
     return
 
 
-def read_arr_help(args):
+def read_arr_help(args) -> Callable[[str, int, Any, str, bool], tuple]:
     """A helper for read_arr used in parallel mode to unpack arguments.
 
     Args:
@@ -82,7 +87,7 @@ def read_arr_help(args):
 def read_arr(
     filepath: str,
     data_size: int,
-    usecols: int = 0,
+    usecols: Any = 0,
     outname: str = None,
     outfile: bool = False,
 ) -> tuple:
@@ -92,7 +97,7 @@ def read_arr(
     Args:
         filepath (str): Path to the data.
         data_size (int): Size of a single element, since they are stacked vertically.
-        usecols (int, optional): Specifies column to import if more than one is available. Defaults to 0.
+        usecols (int or tuple, optional): Specifies column (or columns) to import. Defaults to 0.
         outname (str, optional): To set iff the filename is not the original name in the path. Defaults to None.
         outfile (bool, optional): Name of the file to be saved, is None output is not saved. Defaults to False.
 
@@ -103,13 +108,19 @@ def read_arr(
         os.path.isfile(filepath)
     except:
         print("No such file in {0}".format(filepath))
-    out = np.loadtxt(filepath, usecols=usecols)
-    out = np.squeeze(np.reshape(out, (-1, data_size)))
+
     if outname:
         name = outname
     else:
         # remove extension from filename
         name = os.path.basename(filepath)[:-4]
+
+    out = np.loadtxt(filepath, usecols=usecols)
+    if type(usecols) is tuple:
+        # input is complex
+        out = out[:, 0] + 1j * out[:, 1]
+    out = np.squeeze(np.reshape(out, (-1, data_size)))
+
     if outfile:
         np.save(name + "npy", np.getfromtxt(filepath, usecols=usecols))
         print("Saved as {0}".format(outfile))
