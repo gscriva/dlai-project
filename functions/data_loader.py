ea82bb237e81ae99bd35b0762dd160168acae97b
gscriva <giuseppe.scriva@unicam.it>
2021-01-20 19:16:31 +0100

    Extend Speckle class to handle complex data

--- functions/data_loader.py
+++ functions/data_loader.py
@@ -1,6 +1,7 @@
 import numpy as np
 from math import floor
 from torch.utils.data import Dataset
+from torchvision import transforms
 
 
 class Speckle(Dataset):
@@ -10,17 +11,19 @@ class Speckle(Dataset):
         self,
         data_file: str,
         data_name: str,
-        transform=None,
+        input_size: int = None,
+        transform: transforms.transforms.Compose = None,
         output_name: str = "evalues",
         train: bool = True,
         train_size: float = 0.9,
         seed: int = 0,
     ):
         """
-        Attribute:
+        Args:
             data_file (str): Path to the npz file.
             data_name (str): Key to retrieve the correct array from the file.
-            transform () Compose of transformations to  apply to the dataset. Defaults to None.
+            input_size (int): Size of the input 
+            transform (torchvision.transforms.transforms.Compose) Compose of transformations to  apply to the dataset. Defaults to None.
             output_name (str, optional): Key to get the energy values. Defaults to 'evalues'.
             train (bool, optional): Set True if it has to return the train set. Defaults to True.
             train_size (float, optional): Set the size of training set. Defaults to 0.9.
@@ -34,10 +37,18 @@ class Speckle(Dataset):
         idx[
             np.random.choice(size_ds, floor(size_ds * train_size), replace=False)
         ] = True
+        # for the validation set just take the remaining indexes
         if not train:
             idx = np.logical_not(idx)
-        self.dataset = data[data_name][idx, ...]
+        # for Fourier data most of data are zeros,
+        # so we will use only the non-zero components
+        if input_size is None:
+            input_size = size_ds
+
+        self.dataset = data[data_name][idx, :input_size]
         self.evalues = data[output_name][idx]
+
+        self._get_real_ds()
 
         if transform:
             self.dataset = transform(self.dataset)
@@ -47,3 +58,9 @@ class Speckle(Dataset):
 
     def __getitem__(self, idx):
         return self.dataset[idx, ...], self.evalues[idx]
+
+    def _get_real_ds(self):
+        if self.dataset.dtype == np.complex128:
+            real_ds = np.real(self.dataset)
+            imag_ds = np.imag(self.dataset)
+            self.dataset = np.append(real_ds, imag_ds, axis=1)
