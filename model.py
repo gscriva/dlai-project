2aee60384daaa854ffcb3c904a0a287cc475d231
gscriva <giuseppe.scriva@unicam.it>
2021-01-22 16:08:53 +0100

    Add batchnorm as input layer

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ class MultiLayerPerceptron(nn.Module):
 
         sizes = [input_size] + [150] * layers + [1]
 
-        fc_layers = OrderedDict()
+        fc_layers = OrderedDict({"batch1": nn.BatchNorm1d(input_size)})
         for i in range(len(sizes) - 1):
             fc_layers["linear{0}".format(i)] = nn.Linear(sizes[i], sizes[i + 1])
             if i == len(sizes) - 2:
